9897ea991c4279da3d26ae7aa605610516c99ceb
Adam Brightwell <adam.brightwell@gmail.com>
2017-06-15 10:59:42 -0400

    update initialize replica script for vagrant

--- scripts/vagrant/initialize-replica.sh
+++ scripts/vagrant/initialize-replica.sh
@@ -3,6 +3,7 @@
 PG_BASEBACKUP=/usr/bin/pg_basebackup
 PG_USER=rep
 PG_MASTER_IP=192.168.56.100
+PG_DATA_DIR=/var/lib/pgsql/9.6/data
 
 SUDO='sudo -i PWD=/var/lib/pgsql -u postgres'
 
@@ -10,14 +11,25 @@ echo "Removing Data Directory..."
 $SUDO rm -rf /var/lib/pgsql/9.6/data
 echo "Done."
 
+
+# Initialize the replica data directory based on the master.
+#
+# pg_basebackup parameters (provided for reference):
+#
+# * -P - Show progress information
+# * -R - Write recovery.conf file
+# * -c fast - set fast checkpointing
+# * -X stream - set xlog method to streaming
+# * -h - the ip of the master
+# * -U - the replication user
+# * -D - the directory to receive the base backup.
+#
 echo "Performing Base Backup..."
-$SUDO $PG_BASEBACKUP -h $PG_MASTER_IP -D /var/lib/pgsql/9.6/data -U $PG_USER -v -P
+$SUDO $PG_BASEBACKUP -P -R -c fast -X stream -h $PG_MASTER_IP -U $PG_USER -D $PG_DATA_DIR
 
 echo "Setting up recovery.conf..."
 $SUDO "cat > /var/lib/pgsql/9.6/data/recovery.conf << 'EOF'
-  standby_mode = 'on'
-  primary_conninfo = 'host=$PG_MASTER_IP port=5432 user=$PG_USER sslmode=require'
-  trigger_file = '/tmp/postgresql.trigger'
+  trigger_file = '$PG_DATA_DIR/postgresql.trigger'
 EOF
 "
 echo "Done."
