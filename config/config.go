c0da73ca9ddef650cf8de9814c0cf907c99182d7
Adam Brightwell <adam.brightwell@crunchydata.com>
2017-07-17 14:57:45 -0400

    update copyright header in all source files

--- config/config.go
+++ config/config.go
@@ -1,9 +1,8 @@
 /*
-Copyright 2016 Crunchy Data Solutions, Inc.
+Copyright 2017 Crunchy Data Solutions, Inc.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
 http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
@@ -12,12 +11,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package config
 
 import (
-	//	"bytes"
-	//	"io/ioutil"
-
 	"github.com/spf13/viper"
 
 	"github.com/crunchydata/crunchy-proxy/common"
